dff0e8038d3517b0681bc4b89f99c03ac3b395ef
AndreBaia <mdacriativa@gmail.com>
2016-05-05 01:28:03 -0300

    Testes Arquilliam

--- seguros/src/main/java/br/unibh/seguros/entidades/Usuario.java
+++ seguros/src/main/java/br/unibh/seguros/entidades/Usuario.java
@@ -1,5 +1,6 @@
 package br.unibh.seguros.entidades;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -20,108 +21,143 @@ import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
-@Table(name="tb_usuario")
+@Table(name = "tb_usuario")
 
-public class Usuario {
-	
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public Usuario() {
+	}
+
+	public Usuario(String nome, String login, String senha, String perfil, String cargo, String email,
+			Date dataCadastro, Setor setor) {
+		super();
+		this.nome = nome;
+		this.login = login;
+		this.senha = senha;
+		this.perfil = perfil;
+		this.cargo = cargo;
+		this.email = email;
+		this.dataCadastro = dataCadastro;
+		this.setor = setor;
+	}
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotBlank
 	@Size(min = 8, max = 15)
-	@Pattern (regexp = "[A-z0-9]*")
-	@Column (length=15, nullable=false,unique=true)
+	@Pattern(regexp = "[A-z0-9]*")
+	@Column(length = 15, nullable = false, unique = true)
 	private String login;
-	
+
 	@NotBlank
 	@Size(min = 8, max = 15)
-	@Column (length=100, nullable=false)
+	@Column(length = 100, nullable = false)
 	private String senha;
-	
+
 	@NotNull
 	@Size(min = 5, max = 100)
-	@Pattern(regexp = "[A-zÀ-ú ]*" , message = "Deve conter apenas letras e espaços")
-	@Column (length=100, nullable=false)
+	@Pattern(regexp = "[A-zÀ-ú ]*", message = "Deve conter apenas letras e espaços")
+	@Column(length = 100, nullable = false)
 	private String nome;
-	
+
 	@NotBlank
 	@Pattern(regexp = "[A-zÁ-ú ]")
 	@Size(min = 5, max = 100)
-	@Column (length=30, nullable=false)
+	@Column(length = 30, nullable = false)
 	private String perfil;
-	
+
 	@NotBlank
 	@Pattern(regexp = "[A-zÁ-ú ]")
-	@Size(min = 5, max = 100)	
-	@Column (length=100, nullable=false)
+	@Size(min = 5, max = 100)
+	@Column(length = 100, nullable = false)
 	private String cargo;
-	
+
 	@NotBlank
 	@Email
 	@Size(max = 100)
 
-	@Column (length=100, nullable=false)
+	@Column(length = 100, nullable = false)
 	private String email;
-	
-	@Column(name="data_cadastro", nullable=false)
+
+	@Column(name = "data_cadastro", nullable = false)
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date dataCadastro;
-	
-	@ManyToOne(fetch=FetchType.EAGER)
+
+	@ManyToOne(fetch = FetchType.EAGER)
 	private Setor setor;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getLogin() {
 		return login;
 	}
+
 	public void setLogin(String login) {
 		this.login = login;
 	}
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
 	public String getPerfil() {
 		return perfil;
 	}
+
 	public void setPerfil(String perfil) {
 		this.perfil = perfil;
 	}
+
 	public String getCargo() {
 		return cargo;
 	}
+
 	public void setCargo(String cargo) {
 		this.cargo = cargo;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public Date getDataCadastro() {
 		return dataCadastro;
 	}
+
 	public void setDataCadastro(Date dataCadastro) {
 		this.dataCadastro = dataCadastro;
 	}
+
 	public Setor getSetor() {
 		return setor;
 	}
+
 	public void setSetor(Setor setor) {
 		this.setor = setor;
 	}
